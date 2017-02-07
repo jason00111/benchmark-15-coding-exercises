f4d6f2f6441ecf9caf56511b3f4d2d9f5b73c199
jason00111 <emberleyjason@gmail.com>
2017-02-07 15:01:54 -0800

    fibToDec works, decToFib broken

--- 4.js
+++ 4.js
@@ -16,9 +16,11 @@ function decimalToFibonacci (inputNumber) {
   let number = inputNumber
   const result = []
 
+  fib.reset()
+
   findBiggestFibDivisor(number)
 
-  number -= fib.current
+  number -= fib.prev()
   result.push(1)
 
   while (true) {
@@ -38,30 +40,42 @@ function decimalToFibonacci (inputNumber) {
   return result
 }
 
+function findBiggestFibDivisor (number) {
+  while (number / fib.next() > 1) {}
+  fib.prev()
+  fib.prev()
+}
+
 const fib = {
   previous: 0,
   current: 1,
+  reset: function () {
+    this.previous = 0
+    this.current = 1
+  },
   next: function () {
+    const current = this.current
     const next = this.current + this.previous
     this.previous = this.current
     this.current = next
-    return this.current
+    return current
   },
   prev: function () {
+    const current = this.current
+    if (this.current === 1 && this.previous === 0) {
+      return current
+    }
     const prevPrev = this.current - this.previous
     this.current = this.previous
     this.previous = prevPrev
-    return this.current
+    return current
   }
-}
-
-function findBiggestFibDivisor (number) {
-  while (number / fib.next() > 1) {}
-  return fib.prev()
 }
 
 function fibonacciToDecimal (number) {
   const numberArray = number.toString().split('')
+
+  fib.reset()
 
   return numberArray.reduceRight(
     (accumulator, currentDigit) => accumulator + currentDigit * fib.next(),
