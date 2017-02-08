96220a26ad4287a78094bd5ea3648ea42fa77ae2
jason00111 <emberleyjason@gmail.com>
2017-02-08 13:58:26 -0800

    Better chords

--- 8.js
+++ 8.js
@@ -53,6 +53,13 @@ const C5 = C4 * 2
 const C3 = C4 / 2
 const G3 = G4 / 2
 const B3 = B4 / 2
+const A3 = A4 / 2
+const F2 = F4 / 4
+const G2 = G3 / 2
+const E3 = E4 / 2
+const A2 = A3 / 2
+const F3 = F4 / 2
+const E2 = E3 / 2
 
 oscillator1.frequency.value = C4
 
@@ -90,10 +97,10 @@ while (audioContext.currentTime < 8) {}
 
 gainNode1.gain.value = 0
 
-oscillator1.frequency.value = G3
+oscillator1.frequency.value = G4
 oscillator2.frequency.value = C4
-oscillator3.frequency.value = E4
-oscillator4.frequency.value = G4
+oscillator3.frequency.value = G3
+oscillator4.frequency.value = E2
 
 oscillator2.start()
 oscillator3.start()
@@ -104,18 +111,58 @@ while (audioContext.currentTime < 10) {}
 gainNode1.gain.value = 0.5
 gainNode2.gain.value = 0.5
 
+while (audioContext.currentTime < 11) {}
+
+oscillator1.frequency.value = F4
+oscillator2.frequency.value = C4
+oscillator3.frequency.value = A3
+oscillator4.frequency.value = F2
+
 while (audioContext.currentTime < 12) {}
 
-oscillator2.frequency.value = B3
-oscillator3.frequency.value = D4
+oscillator1.frequency.value = E4
+oscillator2.frequency.value = C4
+oscillator3.frequency.value = G3
+oscillator4.frequency.value = G2
+
+while (audioContext.currentTime < 13) {}
+
+oscillator1.frequency.value = C4
+oscillator2.frequency.value = C4
+oscillator3.frequency.value = E3
+oscillator4.frequency.value = A2
 
 while (audioContext.currentTime < 14) {}
 
-oscillator1.frequency.value = C3
+oscillator1.frequency.value = D4
 oscillator2.frequency.value = C4
-oscillator3.frequency.value = E4
+oscillator3.frequency.value = A3
+oscillator4.frequency.value = F2
+
+while (audioContext.currentTime < 15) {}
+
+oscillator1.frequency.value = D4
+oscillator2.frequency.value = B3
+oscillator3.frequency.value = G3
+oscillator4.frequency.value = G2
+
+while (audioContext.currentTime < 15.5) {}
+
+oscillator1.frequency.value = D4
+oscillator2.frequency.value = B3
+oscillator3.frequency.value = F3
+oscillator4.frequency.value = G2
+
+while (audioContext.currentTime < 16) {}
+
+oscillator1.frequency.value = C4
+oscillator2.frequency.value = C4
+oscillator3.frequency.value = E3
+oscillator4.frequency.value = C3
 
 while (audioContext.currentTime < 18) {}
 
 gainNode1.gain.value = 0
 gainNode2.gain.value = 0
+
+// Rustington -Hubert Parry
