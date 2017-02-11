97d639764a4baf6cd4b5e04cad3e36588eb6ce89
jason00111 <emberleyjason@gmail.com>
2017-02-10 16:30:09 -0800

    ready to go

--- 8.js
+++ 8.js
@@ -1,126 +1,121 @@
-//----- global variables
+////
+// GLOBAL VARIABLES
+//
 
-const audioContext = new AudioContext()
+  const audioContext = new AudioContext()
 
-const gainNode = audioContext.createGain()
-gainNode.gain.value = 0.5
-gainNode.connect(audioContext.destination)
+  const gainNode = audioContext.createGain()
+  gainNode.gain.value = 0.5
+  gainNode.connect(audioContext.destination)
 
-base = 220
+  let base = 220
 
-let oscillators = Array(2).fill(null)
+  let oscillators = Array(2).fill(null)
 
-//----- functions
+//
+//
+////
 
-function initializeOcillators (numberOfOcillators) {
-  oscillators = oscillators.map(() => audioContext.createOscillator())
+////
+// GLOBAL FUNCTIONS
+//
+//---> initializeOcillators and friends
+//
 
-  oscillators.forEach(oscillators => {
-    oscillators.setPeriodicWave(normalWave())
-    oscillators.connect(gainNode)
-  })
+  function initializeOcillators (numberOfOcillators) {
+    oscillators = oscillators.map(() => audioContext.createOscillator())
 
-  for (let index = 0; index < numberOfOcillators; index++) {
-    oscillators[index].start()
-  }
-}
+    oscillators.forEach(oscillators => {
+      oscillators.setPeriodicWave(normalWave())
+      oscillators.connect(gainNode)
+    })
 
-const normalWave = () => { //!!^^
-  const harmonicLevels = [ 0, 1, 0.7, 0.5, 0.3, 0.2, 0.1 ]
-
-  const real = new Float32Array(2)
-  const imag = new Float32Array(2)
-
-  harmonicLevels.forEach((level, index) => {
-    real[index] = level
-    imag[index] = 0
-  })
-
-  return audioContext.createPeriodicWave(real, imag)
-}
-
-const chords = [
-  [base * 3/4, base],
-  // [base * 3/4, base * 3/4 * 3/2], //9/8
-  [base * 3/4, base * 3/4 * 5/4], //15/16
-  [base * 1/2, base]
-]
-
-
-const chords2 = [
-  [base * 3/2, base * 3/2 * 3/2], //9/8
-  [base * 1, base * 2]
-]
-
-const chords3 = [
-  [base * 3/2, base * 3/2 * 5/4], //15/16
-  [base * 1, base * 2]
-]
-
-const chords4 = [
-  [base * 3/2, base * 2],
-  [base * 1,   base * 2]
-]
-
-const chords5 = [
-  [base * 3/4, base], //2
-  [base * 1/2, base]
-]
-
-
-let chords6 = [
-  [3/2, 2],
-  [1,   2]
-]
-
-chords6 = chords6.map(time => time.map(ratio => ratio * base))
-// ratio===chord
-
-initializeOcillators(2)
-
-playTheNotes(chords6.map(notes => [2, ...notes]))
-
-
-//----- more functions
-
-function volumeOff () {
-  gainNode.gain.value = 0
-}
-
-function volumeOn (volume = 0.5) {
-  gainNode.gain.value = volume
-}
-
-function showFrequencies() {
-  const sortedFrequencies = Array.from(arguments).sort((a, b) => a - b)
-  console.log(...sortedFrequencies)
-}
-
-function playTheNotes (notes) {
-  volumeOn()
-
-  let now
-  for (let note of notes) {
-    showFrequencies(
-      note[1],
-      note[1] * 2,
-      note[1] * 3,
-      note[1] * 4,
-      note[1] * 5,
-      note[1] * 6,
-      note[2],
-      note[2] * 2,
-      note[2] * 3,
-      note[2] * 4,
-      note[2] * 5,
-      note[2] * 6
-    )
-    oscillators.forEach(
-      (oscillator, index) => oscillator.frequency.value = note[index + 1]
-    )
-    now = audioContext.currentTime
-    while (audioContext.currentTime < now + note[0]) {}  // BLOCKING
+    for (let index = 0; index < numberOfOcillators; index++) {
+      oscillators[index].start()
+    }
   }
 
-  volumeOff()
-}
+  const normalWave = () => { //!!^^
+    const harmonicLevels = [ 0, 1, 0.7, 0.5, 0.3, 0.2, 0.1 ]
+
+    const real = new Float32Array(2)
+    const imag = new Float32Array(2)
+
+    harmonicLevels.forEach((level, index) => {
+      real[index] = level
+      imag[index] = 0
+    })
+
+    return audioContext.createPeriodicWave(real, imag)
+  }
+
+//
+//---> playTheNotes and friends
+//
+
+  function volumeOff () {
+    gainNode.gain.value = 0
+  }
+
+  function volumeOn (volume = 0.5) {
+    gainNode.gain.value = volume
+  }
+
+  function showFrequencies() {
+    const sortedFrequencies = Array.from(arguments).sort((a, b) => a - b)
+    console.log(...sortedFrequencies)
+  }
+
+  function playTheNotes (notes) {
+    volumeOn()
+
+    let now
+    for (let note of notes) {
+      showFrequencies(
+        note[1],
+        note[1] * 2,
+        note[1] * 3,
+        note[1] * 4,
+        note[1] * 5,
+        note[1] * 6,
+        note[2],
+        note[2] * 2,
+        note[2] * 3,
+        note[2] * 4,
+        note[2] * 5,
+        note[2] * 6
+      )
+      oscillators.forEach(
+        (oscillator, index) => oscillator.frequency.value = note[index + 1]
+      )
+      now = audioContext.currentTime
+      while (audioContext.currentTime < now + note[0]) {}  // BLOCKING
+    }
+
+    volumeOff()
+  }
+
+//
+//
+////
+
+
+////
+// THINGS ARE HAPPENING HERE
+//
+
+  let chords = [
+    [3/2, 2],
+    [1,   2]
+  ]
+
+  initializeOcillators(2)
+
+  playTheNotes( // BLOCKING
+    chords
+      .map(ratios => ratios.map(ratio => ratio * base))
+      .map(frequencies => [2, ...frequencies])
+  )
+
+//
+////
